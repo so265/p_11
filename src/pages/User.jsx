c81b2bf0467e2b7c7d622a400d2fd69763d7f742
so265 <undia42@gmail.com>
2023-10-12 14:01:44 +0200

    creation component welcomeBack

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import WelcomeBack from '../components/WelcomeBack.jsx'
 
 function User() {
   return (
-    <div>User</div>
+    <div>
+      <WelcomeBack />
+    </div>
   )
 }
 
