e74f42bc5612d4432e45a74d58d4cbc9e4b3d0d4
so265 <undia42@gmail.com>
2023-10-17 12:43:05 +0200

    signout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux'; // Importez useSelector pour accéder à l'état Redux
+import { useSelector, useDispatch } from 'react-redux'; // Importez useSelector pour accéder à l'état Redux
 import logo from '../assets/argentBankLogo.webp';
+import { updateToken } from '../reducers/userReducer.jsx'; 
+
 import '../styleComponents/Header.scss';
 
 function Header() {
   // Utilisez useSelector pour accéder à l'état Redux
   const isAuthenticated = useSelector(state => state.user.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const handleSignOut = () => {
+    // Déclenchez l'action Redux pour mettre à jour l'état d'authentification
+    dispatch(updateToken(false));
+  };
 
   return (
     <div className="main-nav">
@@ -18,7 +26,7 @@ function Header() {
         />
       </Link>
 
-      <Link className="main-nav-item" to="/Login">
+      <Link className="main-nav-item" to="/Login" onClick={handleSignOut}>
         <i className="fa fa-user-circle"></i>
         {isAuthenticated ? 'Sign Out' : 'Sign In'}
       </Link>
