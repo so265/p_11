31f3fa7ce4c371515b3bece7d8afed290c32029a
so265 <undia42@gmail.com>
2023-10-23 23:51:39 +0200

    error message

--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -14,13 +14,16 @@ function Formulaire() {
   const [formData, setFormData] = useState({ email: '', password: '' });
   const dispatch = useDispatch();
   const navigate = useNavigate(); 
+  
+  // Ajout d'un état pour gérer le message d'erreur
+  const [errorMessage, setErrorMessage] = useState(null);
 
   // Fonction pour gérer la soumission du formulaire de connexion.
   const handleSignIn = async () => {
     try {
       const response = await loginToAPI(formData);
 
-       // Vérifie la réponse de l'API.
+      // Vérifie la réponse de l'API.
       if (response.status === 200 && response.body) {
         const { token } = response.body;
         console.log('Token from API:', token);
@@ -36,7 +39,7 @@ function Formulaire() {
         // Vérifie si les données de profil sont valides
         if (userProfileData && userProfileData.body) {
           const { firstName, lastName, email } = userProfileData.body;
-          const userName = `${firstName} ${lastName}`;  // Créeation du nom complet de l'utilisateur.
+          const userName = `${firstName} ${lastName}`;  // Création du nom complet de l'utilisateur.
           
           dispatch(setUser({ name: userName, email })); // Mise à jour des données de l'utilisateur dans le store Redux.
         }
@@ -44,10 +47,11 @@ function Formulaire() {
         dispatch(setAuthenticated(true));  // Définit l'authentification comme réussie dans le store Redux.
         navigate('/user'); // Redirige l'utilisateur vers la page "/user".
       } else {
-        console.error('Invalid API response or undefined user.');
+        throw new Error("Invalid API response");
       }
     } catch (error) {
       console.error('Error during sign-in:', error);
+      setErrorMessage("Le nom d'utilisateur ou le mot de passe est incorrect.");
     }
   }
 
@@ -56,6 +60,10 @@ function Formulaire() {
       <section className="sign-in-content main">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
+
+        {/* Affiche le message d'erreur si présent */}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        
         <form>
           <div className="input-wrapper">
             <label htmlFor="email">Email</label>
