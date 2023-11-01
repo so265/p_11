286e1e94cdf8870769675fbb7d404dce2c611889
so265 <undia42@gmail.com>
2023-11-01 17:20:09 +0100

    added error message formulaire

--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -4,31 +4,45 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { updateToken } from '../reducers/authentificationReducer.jsx';
 import { setAuthenticated, setUser } from '../reducers/userReducer.jsx'; // Import de l'action setUser
-import { loginToAPI } from '../api/authApi'; 
+import { loginToAPI } from '../api/authApi';
 import { fetchUserProfile } from '../action/userAction';
 
 import '../styleComponents/Formulaire.scss';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 
 function Formulaire() {
   const [formData, setFormData] = useState({ email: '', password: '' });
   const dispatch = useDispatch();
-  const navigate = useNavigate(); 
-  
-  // Ajout d'un état pour gérer le message d'erreur
-  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(null); // Ajout d'un état pour gérer le message d'erreur
+
+  // Vérification de la validité de l'email
+  const validateEmail = (email) => {
+    const regex = /\S+@\S+\.\S+/;
+    return regex.test(email);
+  };
 
   // Fonction pour gérer la soumission du formulaire de connexion.
   const handleSignIn = async () => {
+    // Je m'assure que l'email a un format valide.
+    if (!validateEmail(formData.email)) {
+      setErrorMessage("Format d'email invalide.");
+      return; // J'arrête la soumission si l'email n'est pas valide.
+    }
+
+    // Je vérifie que le mot de passe a la longueur requise.
+    if (formData.password.length < 6) {
+      setErrorMessage("Le mot de passe doit contenir au moins 6 caractères.");
+      return; // J'arrête la soumission si le mot de passe est trop court.
+    }
+
     try {
       const response = await loginToAPI(formData);
 
-      // Vérifie la réponse de l'API.
-      if (response.status === 200 && response.body) {
+      if (response.status === 200 && response.body) { // Vérifie la réponse de l'API.
         const { token } = response.body;
-        
-      // Mise à jour du token dans le store Redux.
-        dispatch(updateToken(token));
+
+        dispatch(updateToken(token));  // Mise à jour du token dans le store Redux.
 
         // Récupère les données de profil de l'utilisateur depuis l'API
         const userProfileData = await dispatch(fetchUserProfile(token));
@@ -39,7 +53,7 @@ function Formulaire() {
         if (userProfileData && userProfileData.body) {
           const { firstName, lastName, email } = userProfileData.body;
           const userName = `${firstName} ${lastName}`;  // Création du nom complet de l'utilisateur.
-          
+
           dispatch(setUser({ name: userName, email })); // Mise à jour des données de l'utilisateur dans le store Redux.
         }
 
@@ -61,8 +75,8 @@ function Formulaire() {
         <h1>Sign In</h1>
 
         {/* Affiche le message d'erreur si présent */}
-        {errorMessage && <p className="error-message">{errorMessage}</p>} 
-        
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+
         <form>
           <div className="input-wrapper">
             <label htmlFor="email">Email</label>
